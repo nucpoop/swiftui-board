22838c55d2b98ca08ad65d7137db7993b0f51892
nucpoop <kjm5546@gmail.com>
2023-04-01 18:36:17 +0900

    Initial Commit

---
+++ swiftui-board/swiftui_boardApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_boardApp.swift
+//  swiftui-board
+//
+//  Created by nucpoop on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_boardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
